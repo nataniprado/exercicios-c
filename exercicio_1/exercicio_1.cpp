1813cf9f14e45cc98770312f6f6df9ea3cef1fc6
Natani Prado <nataniprado26@gmail.com>
2022-02-20 22:48:49 -0300

    Init Repository

---
+++ exercicio_1/exercicio_1.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main() {
+    int numero = 0;
+    printf("Digite um numero: ");
+    scanf("%i",&numero);
+    printf("O numero digitado foi: %i.", numero);
+    return 0;
+} 
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero = 0;
+    cout << "Digite um numero: ";
+    cin >> numero;
+    cout << "O numero digitado foi: " << (numero + 2);
+    return 0;
+}
